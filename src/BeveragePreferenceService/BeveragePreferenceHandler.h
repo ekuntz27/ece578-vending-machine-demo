f707d4af48904b2f19b055b56f98a7ae1ce302ba
Eva Kuntz <evakuntz@iastate.edu>
2021-02-23 12:10:31 -0600

    Adding BeveragePreferenceService class and config files

---
+++ src/BeveragePreferenceService/BeveragePreferenceHandler.h
@@ -0,0 +1,47 @@
+#ifndef VENDING_MACHINE_MICROSERVICES_BEVERAGEPREFERENCEHANDLER_H
+#define VENDING_MACHINE_MICROSERVICES_BEVERAGEPREFERENCEHANDLER_H
+
+#include <iostream>
+#include <string>
+#include <regex>
+#include <future>
+
+#include "../../gen-cpp/BeveragePreferenceService.h"
+
+#include "../ClientPool.h"
+#include "../ThriftClient.h"
+#include "../logger.h"
+
+namespace vending_machine{
+    class BeveragePreferenceServiceHandler : public BeveragePreferenceServiceIf {
+        // Private class variables
+        string hotBeverages [3] = {"cappuccino", "latte", "espresso"};
+        string coldBeverages [3] = {"lemondate", "ice tea", "soda"};
+
+        public:
+        BeveragePreferenceServiceHandler();
+        ~BeveragePreferenceServiceHandler() override=default;
+
+        string GetBeverage(BeverageType::type btype) override;
+    };
+
+    // Constructor
+    BeveragePreferenceServiceHandler::BeveragePreferenceServiceHandler() {
+
+    }
+
+    string BeveragePreferenceServiceHandler::GetBeverage(BeverageType::type btype){
+        printf("GetBeverage\n");
+        if(btype == BeverageType::type::HOT){
+            return hotBeverages[rand()%3];
+        }else{
+            return coldBeverages[rand()%3];
+        }
+
+
+    }
+   
+
+} // namespace vending_machine
+
+#endif // VENDING_MACHINE_MICROSERVICES_BEVERAGEPREFERENCEHANDLER_H
